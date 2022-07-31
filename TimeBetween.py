68f9cbd57148620ec69fd262814ea4ff73231343
Logan <logansingleton3@gmail.com>
2022-07-30 22:21:21 -0600

    Check if time is between parameters file added

---
+++ TimeBetween.py
@@ -0,0 +1,15 @@
+from datetime import datetime, time
+
+def is_time_between(begin_time, end_time, check_time=None):
+    # If check time is not given, default to current UTC time
+    check_time = check_time or datetime.utcnow().time()
+    if begin_time < end_time:
+        return check_time >= begin_time and check_time <= end_time
+    else: # crosses midnight
+        return check_time >= begin_time or check_time <= end_time
+
+if __name__=='__main__':
+    # Original test case from OP
+    is_time_between()
+
+
