eabc40f2a135cd8ace838cb1cd5f296c10298f0d
Logan <logansingleton3@gmail.com>
2022-08-11 14:09:24 -0600

    t status NSIS

--- Setup.py
+++ Setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup,find_packages
 
-print(find_packages())
+setup(
+    name='PyAutoRaid',
+    description='Automate Raid: Shadow Legends',
+    author='Snoop',
+    version='0.1.0',
+    packages=find_packages(include=['PyAutoRaid','PyAutoRaid.*'])
+    
+)
