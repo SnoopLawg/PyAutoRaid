9e5878047704ec066c96986071aed71a30504d18
Logan <logansingleton3@gmail.com>
2023-04-05 16:17:27 -0600

    v1.0.0-Beta

--- Modules/OpenRaid.py
+++ Modules/OpenRaid.py
@@ -39,101 +39,16 @@ def openRaid():
         time.sleep(30)
         # TODO: Fix the clicks to open raid
     elif operating == "Windows":
-        PATH_PP = "~\AppData\Local\PlariumPlay\PlariumPlay.exe"
-        FULL_PATH_PP = os.path.expanduser(PATH_PP)
-        # PATH_RSL = (
-        #     "~\\AppData\\Local\\PlariumPlay\\StandAloneApps\\raid\\36484\\Raid.exe"
-        # )
-        # FULL_PATH_RSL = os.path.expanduser(PATH_PP)
-        # os.startfile("C:\Program Files\RSL_Helper_X64\RSLHelper.exe")
-        os.startfile(FULL_PATH_PP)
-        # skips after while
         all_windows = pygetwindow.getAllTitles()
-        while "Plarium Play" not in all_windows:
-            all_windows = pygetwindow.getAllTitles()
-            print("Waiting for Plarium Play to open")
-            time.sleep(1)
-        # time.sleep(5)
-        # os.startfile(FULL_PATH_RSL)
-        # count = 0
-        # while "Raid: Shadow Legends" not in all_windows or count == 30:
-        #     all_windows = pygetwindow.getAllTitles()
-        #     print("Waiting for Raid to open")
-        #     time.sleep(1)
-        #     count += 1
-        while (
-            pyautogui.locateOnScreen(
-                ASSETS_PATH + "\\MyLibraryPP.png",
-                confidence=0.8,
-            )
-            is None
-        ):
-            print("waiting to click library")
-            time.sleep(1)
-        time.sleep(1.5)
-        PPlibraryx, PPlibraryy = pyautogui.locateCenterOnScreen(
-            ASSETS_PATH + "\\MyLibraryPP.png",
-            confidence=0.8,
+        subprocess.Popen(
+            [
+                os.path.join(os.getenv("LOCALAPPDATA"), "PlariumPlay\PlariumPlay.exe"),
+                "--args",
+                "-gameid=101",
+                "-tray-start",
+            ]
         )
-        pyautogui.click(PPlibraryx, PPlibraryy)
-        with open("log.txt", mode="a") as file:
-            file.write("\n clicking library")
-        # wait till plarium play button becomes available
-        time.sleep(3.5)
-        if (
-            pyautogui.locateOnScreen(
-                ASSETS_PATH + "\\PPlay.png",
-                confidence=0.8,
-            )
-            != None
-        ):
-            while (
-                pyautogui.locateOnScreen(
-                    ASSETS_PATH + "\\PPlay.png",
-                    confidence=0.8,
-                )
-                == None
-            ):
-                print("waiting to click play")
-            PPlayx, PPlayy = pyautogui.locateCenterOnScreen(
-                ASSETS_PATH + "\\PPlay.png", confidence=0.8
-            )
-            time.sleep(1)
-            pyautogui.click(PPlayx, PPlayy)
-            with open("log.txt", mode="a") as file:
-                file.write("\n playing PP")
-        else:
-            win = pygetwindow.getWindowsWithTitle("Plarium Play")[0]
-            win.size = (900, 600)
-            win.moveTo(0, 0)
-            time.sleep(1)
-            pyautogui.click(84, 240)
-            time.sleep(5)
-            while (
-                pyautogui.locateOnScreen(
-                    ASSETS_PATH + "\\PPlay.png",
-                    confidence=0.8,
-                )
-                == None
-            ):
-                time.sleep(1)
-                pyautogui.click(84, 240)
-                pyautogui.click(953, 139)
-                time_out = 0
-                all_windows = pygetwindow.getAllTitles()
-                if "Raid: Shadow Legends" in all_windows:
-                    break
-                time_out += 1
-                if time_out >= 100:
-                    print("raid never opened lol")
-                    quitAll()
-                    import Main
-
-                    Main.main()
-
-    with open("log.txt", mode="a") as file:
-        file.write("\n files opening")
-    time_out = 0
+        time_out = 0
     while (
         pyautogui.locateOnScreen(
             ASSETS_PATH + "\\exitAdd.png",
